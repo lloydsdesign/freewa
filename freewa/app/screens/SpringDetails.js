79375834452dc5233690dd8ad457df664291ea01
Ricardo Čerljenko <ricardo.cerljenko@hotmail.com>
2017-04-03 17:09:13 +0200

    spring details

--- freewa/app/screens/SpringDetails.js
+++ freewa/app/screens/SpringDetails.js
@@ -3,28 +3,65 @@ import React, {
 } from 'react';
 
 import {
-  StyleSheet,
-  Text,
-  View
+  ScrollView
 } from 'react-native';
 
-export default class SpringDetails extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Hello World!</Text>
-      </View>
-    );
-  }
-}
+import {
+  Row,
+  Subtitle,
+  Text,
+  Title,
+  View,
+  Image,
+  Tile,
+  Divider
+} from '@shoutem/ui';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 20,
-  },
-});
+import { InlineMap } from '@shoutem/ui-addons';
+import { NavigationBar } from '@shoutem/ui/navigation';
+
+
+export default class SpringDetails extends Component
+{
+	render()
+	{
+		const { marker } = this.props;
+		  
+		return (
+			<ScrollView style={{marginTop: -1}}>
+				<NavigationBar title={marker.title.toUpperCase()} />
+				
+				<Image styleName="large-banner" source={{ uri: marker.image }}>
+					<Tile>
+						<Title>{marker.title}</Title>
+						<Subtitle>{marker.type}</Subtitle>
+					</Tile>
+				</Image>
+
+				<Divider styleName="line" />
+
+				<Row>
+					<View style={{flex: 1}}>
+					  <Subtitle>SPRING DESCRIPTION</Subtitle>
+					  <Text />
+					  <Text>{marker.description}</Text>
+					</View>
+				</Row>
+
+				<View styleName="large-banner">
+					<InlineMap
+						initialRegion={{
+							latitude: marker.latitude,
+							longitude: marker.longitude,
+							latitudeDelta: 0.03,
+							longitudeDelta: 0.03
+						}}
+						markers={[marker]}
+						selectedMarker={marker}
+						style={{height: 160}}
+					/>
+				</View>
+			</ScrollView>
+		);
+	}
+}
