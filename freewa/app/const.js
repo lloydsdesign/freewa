c9493b9de4e699c4ba474ed2cb53a71e1fd6d98c
Ricardo Čerljenko <ricardo.cerljenko@hotmail.com>
2017-04-29 07:36:44 +0200

    new cms domain

--- freewa/app/const.js
+++ freewa/app/const.js
@@ -12,7 +12,7 @@ import {
 const turf = require('turf');
 
 const jsonGuard = String.fromCharCode(0);
-const CMS_BASE = 'http://freewa-back.lloyds-design.hr/';
+const CMS_BASE = 'http://admin.freewa.org/';
 const CMS_REST = CMS_BASE +'manage.php';
 const MAX_UPLOAD_SIZE = 1024 * 1024 * 5;
 
